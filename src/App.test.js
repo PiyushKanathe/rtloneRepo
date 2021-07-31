2dfffed446f362f6a32a5192bdd703a1473cdc99
Piyush Kanathe <dit.piyush@gmail.com>
2021-07-31 17:49:13 +0530

    create and test color button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has the correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+  let colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  // Check the button 
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+  // click the button
+  fireEvent.click(colorButton);
+  // expect the button to have changed its olor to blue
+  expect(colorButton).toHaveStyle({backgroundColor : 'blue'});
+  // expect the button to have changed text 'change to red
+  expect(colorButton.textContent).toBe('Change to red');
 });
